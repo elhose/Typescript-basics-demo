5e0bd4ce398917537b8756780808f9597becdee4
Jacek <jaceksawko95@gmail.com>
2020-06-28 16:06:08 +0100

    added inpuit validation and form clearing after submitting

--- src/app.ts
+++ src/app.ts
@@ -48,9 +48,37 @@ class ProjectInput {
 	}
 
 	@Autobind
-	private submitHandler(event: Event){
+	private submitHandler(event: Event) {
 		event.preventDefault();
-		console.log(this.titleInputElement.value)
+		const userInput = this.gatherUserInput();
+		if (userInput) {
+			const [title, description, people] = userInput;
+			// console.log([title, description, people]);
+			this.clearFormsAfterSubmiting();
+		}
+	}
+
+	private clearFormsAfterSubmiting() {
+		this.titleInputElement.value = "";
+		this.descriptionInputElement.value = "";
+		this.peopleInputElement.value = "";
+	}
+
+	private gatherUserInput(): [string, string, number] | void {
+		const enteredTitle = this.titleInputElement.value;
+		const enteredDescription = this.descriptionInputElement.value;
+		const enteredPeople = this.peopleInputElement.value;
+
+		if (
+			enteredTitle.trim().length === 0 ||
+			enteredDescription.trim().length === 0 ||
+			enteredPeople.trim().length === 0
+		) {
+			alert(`Invalid input, please try again!`);
+			return;
+		} else {
+			return [enteredTitle, enteredDescription, +enteredPeople];
+		}
 	}
 
 	private configure(){
