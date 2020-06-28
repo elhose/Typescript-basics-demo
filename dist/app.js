ded6317544e2cc50a76fae62614acde522c86d31
Jacek <jaceksawko95@gmail.com>
2020-06-28 14:58:32 +0100

    rendered project form

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,15 @@
 "use strict";
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        //render form from template
+        const importedForms = document.importNode(this.templateElement.content, true);
+        this.formElement = importedForms.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
+    }
+}
+const projectInput = new ProjectInput();
---
+++ src/app.ts
@@ -0,0 +1,26 @@
+class ProjectInput {
+	templateElement: HTMLTemplateElement;
+	hostElement: HTMLDivElement;
+	formElement: HTMLFormElement;
+
+	constructor() {
+		this.templateElement = document.getElementById(
+			"project-input"
+		)! as HTMLTemplateElement;
+		this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+		//render form from template
+		const importedForms = document.importNode(
+			this.templateElement.content,
+			true
+		);
+		this.formElement = importedForms.firstElementChild as HTMLFormElement;
+		this.attach();
+	}
+
+	private attach() {
+		this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
+	}
+}
+
+const projectInput: ProjectInput = new ProjectInput();
