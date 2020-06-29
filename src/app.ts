38b7b9ba8b1fb7380937c181b67285caed72623c
Jacek Sawko <jacek.sawko@ttpsc.pl>
2020-06-29 16:50:05 +0200

    visual fix for correct form while displaying number of people

--- src/app.ts
+++ src/app.ts
@@ -147,6 +147,14 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     private project: Project;
 
+    get persons(){
+        if (this.project.people === 1) {
+            return `1 person`
+        }else {
+            return `${this.project.people} persons`
+        }
+    }
+
     constructor(hostId: string, project:Project) {
         super("single-project", hostId, "beforeend", project.id);
         this.project = project;
@@ -160,7 +168,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 
     protected renderContent() {
         this.element.querySelector("h2")!.textContent = this.project.title;
-        this.element.querySelector("h3")!.textContent = this.project.people.toString();
+        this.element.querySelector("h3")!.textContent = this.persons + " assigned";
         this.element.querySelector("p")!.textContent = this.project.description;
     }
 }
@@ -269,7 +277,7 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
         const peopleValidatable: Validatable = {
             value: +enteredPeople,
             required: true,
-            min: 1,
+            min: 0,
             max: 5
         }
 
