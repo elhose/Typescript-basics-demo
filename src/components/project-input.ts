1b677103d613c99e93f2c1483078b26bdfe04dd4
Jacek Sawko <jacek.sawko@ttpsc.pl>
2020-06-30 11:15:14 +0200

    Removed .js extensions from imports

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,7 +1,7 @@
-import {Component} from "./base-component.js";
-import {Autobind as ATB} from "../decorators/autobind.js";
-import {projectState} from "../state/project-state.js";
-import * as Validation from "../util/validation.js";
+import {Component} from "./base-component";
+import {Autobind as ATB} from "../decorators/autobind";
+import {projectState} from "../state/project-state";
+import * as Validation from "../util/validation";
 
 export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     titleInputElement: HTMLInputElement;
